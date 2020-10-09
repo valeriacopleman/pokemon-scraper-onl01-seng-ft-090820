a6458aebe24109095ef5e6c83a01ed01751b1a29
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-09 16:00:45 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,7 +9,14 @@ class Pokemon
     @db = db 
   end
   
-  def save 
-    
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album) 
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.album)
+ 
+  end
   
 end
